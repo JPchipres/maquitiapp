76581a978235212bc19744f3f598bc1a549e896d
Hector <hectorjesus029@gmail.com>
2023-10-10 07:44:42 -0600

    Vistas - Cambio de contraseña

---
+++ app/src/main/java/com/idevs/maquitiapp/codigo_otp.java
@@ -0,0 +1,14 @@
+package com.idevs.maquitiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class codigo_otp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_codigo_otp);
+    }
+}
---
+++ app/src/main/java/com/idevs/maquitiapp/password_changed.java
@@ -0,0 +1,14 @@
+package com.idevs.maquitiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class password_changed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_changed);
+    }
+}
