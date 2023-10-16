362ba49121dbc107a6d2ea720e3dca63ea4dbe7e
sebastian gomez calvario <sgomez4@ucol.mx>
2023-10-16 12:34:28 -0600

    Las 4 vistas que me tocaron

---
+++ app/src/main/java/com/idevs/maquitiapp/main_sin_registro_asociacion.java
@@ -0,0 +1,14 @@
+package com.idevs.maquitiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class main_sin_registro_asociacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_sin_registro_asociacion);
+    }
+}
---
+++ app/src/main/java/com/idevs/maquitiapp/perfil_usuario.java
@@ -0,0 +1,14 @@
+package com.idevs.maquitiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class perfil_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perfil_usuario);
+    }
+}
